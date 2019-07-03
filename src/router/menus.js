c2332522036eec0d7a3783c18ff07d4ce7a5ae7c
nick <1060768890@qq.com>
2019-07-03 19:02:02 +0800

    init

---
+++ src/router/menus.js
@@ -0,0 +1,20 @@
+export default [
+  // {
+  //   path: '/testing',
+  //   menuCode: 'testing',
+  //   menuIcon: 'md-funnel',
+  //   menuLabel: '测试',
+  //   notCache: true,
+  //   component: 'components/main',
+  //   children: [
+  //     {
+  //       path: 'menu1',
+  //       menuCode: 'menu1',
+  //       menuIcon: 'md-funnel',
+  //       menuLabel: '菜单1',
+  //       notCache: true,
+  //       component: 'view/testing/test1.vue'
+  //     }
+  //   ]
+  // }
+]
